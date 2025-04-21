bf75e16b8f914ef463e79470410c9fa2e3c07e40
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-04-21 04:40:22 +0000

    add model utility module

---
+++ src/pamiq_vision_exp/models/utils.py
@@ -0,0 +1,8 @@
+"""Utility tools for model definitions."""
+
+type size_2d = int | tuple[int, int]
+
+
+def size_2d_to_int_tuple(size: size_2d) -> tuple[int, int]:
+    """Convert `size_2d` type to int tuple."""
+    return (size, size) if isinstance(size, int) else (size[0], size[1])
