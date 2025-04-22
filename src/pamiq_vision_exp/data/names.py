d24b6b352335ae0dd148574fd4816b30711951cc
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-04-22 01:54:55 +0000

    add names module in data

---
+++ src/pamiq_vision_exp/data/names.py
@@ -0,0 +1,13 @@
+from enum import StrEnum, auto
+
+
+class BufferNames(StrEnum):
+    """Enumerates all buffer names in the experiments."""
+
+    IMAGE = auto()
+
+
+class KeyNames(StrEnum):
+    """Enumerates all data key names in the experiments."""
+
+    IMAGE = auto()
