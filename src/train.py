6f1155dd712227fa1216f5c6bdc6c4e6b483ee8c
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-05-08 05:56:06 +0000

    feat(hydra): add basic train script

---
+++ src/train.py
@@ -0,0 +1,17 @@
+import hydra
+import rootutils
+from omegaconf import DictConfig
+from pamiq_core import LaunchConfig, launch
+
+PROJECT_ROOT = rootutils.setup_root(
+    __file__, indicator=".project-root", pythonpath=True
+)
+
+
+@hydra.main("../configs", "train.yaml", version_base="1.3")
+def main(cfg: DictConfig) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
