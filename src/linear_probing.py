abd32c778dc41551d73a8e512b9dbe12b8f47a12
zassou65535 <gravity@kuf.biglobe.ne.jp>
2025-07-26 23:16:02 +0900

    add some features

--- src/linear_probing.py
+++ src/linear_probing.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn as nn
 from omegaconf import DictConfig, OmegaConf
 from pamiq_core import launch
+from torch.utils.data import DataLoader
 
 from exp.aim_utils import flatten_config, set_global_run
 from exp.instantiations import (
@@ -16,6 +17,7 @@ from exp.instantiations import (
     instantiate_models,
     instantiate_trainers,
 )
+from exp.models.names import ModelName
 from exp.oc_resolvers import register_custom_resolvers
 
 register_custom_resolvers()
@@ -57,8 +59,42 @@ def main(cfg: DictConfig) -> None:
 
 
 def train(cfg: DictConfig, run: aim.Run) -> None:
-    # target_pretrained_model: nn.Module = hydra.utils.instantiate(cfg.models)
-    pass
+    # create pretrained model
+    pretrained_model: nn.Module = hydra.utils.instantiate(cfg.models)[
+        ModelName.JEPA_TARGET_ENCODER
+    ]
+    pretrained_model.load_state_dict(
+        torch.load(cfg.pretrained_weight, weights_only=True)
+    )
+    pretrained_model = pretrained_model.to(cfg.device)
+    # define linear probing head
+    classifier = nn.Linear(cfg.models.embed_dim, 100, bias=True)
+    classifier = classifier.to(cfg.device)
+
+    optimizer = hydra.utils.instantiate(cfg.partial_optimizer)(classifier.parameters())
+    dataloader = DataLoader(
+        dataset=hydra.utils.instantiate(cfg.datasets.cifer100),
+        batch_size=cfg.batch_size,
+        shuffle=True,
+        drop_last=True,
+        pin_memory=True,
+    )
+    for _ in range(cfg.max_epochs):
+        images: torch.Tensor  # [batch_size, channels, height, width]
+        labels: torch.Tensor  # [batch_size]
+        for images, labels in dataloader:
+            images = images.to(cfg.device)
+            labels = labels.to(cfg.device)
+            with torch.no_grad():
+                features = pretrained_model(
+                    images
+                )  # [batch_size, n_patches, embed_dim]
+                features = features.mean(dim=1)  # [batch_size, embed_dim]
+            logits = classifier(features)  # torch.Size([batch_size, n_classes])
+            loss = torch.nn.functional.cross_entropy(logits, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
 
 if __name__ == "__main__":
